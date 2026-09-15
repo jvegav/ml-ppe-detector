26cd962622f8a7d2eed09f3415180a6fe60ea782
Josue Vega <josuevegavalbuena@gmail.com>
2026-09-15 22:12:07 +0200

    templates for each microservice

---
+++ ms-auth/src/main/java/com/example/mi_springboot/MiSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.mi_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiSpringbootApplication.class, args);
+	}
+
+}
